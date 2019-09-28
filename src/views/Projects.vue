50a8c95bb13d948559c52845ccdb855b8b75926f
Mantasbl <mantas.baltakis@gmail.com>
2019-09-28 15:41:42 +0100

    Designed dashboard view with dummy project data

---
+++ src/views/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="projects">
+    <v-subheader class="grey--text">Projects</v-subheader>
+
+    <v-container class=" my-5">
+        Content
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="team">
+    <v-subheader class="grey--text">Team</v-subheader>
+
+    <v-container class=" my-5">
+        Content
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
