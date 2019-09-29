fa014fb994e92e57f6afca0fe60dfab7e16046d3
Mantasbl <mantas.baltakis@gmail.com>
2019-09-29 14:33:54 +0100

    Added projects sorting and their button tooltips

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,28 @@
     <v-subheader class="grey--text">Dashboard</v-subheader>
 
     <v-container class=" my-5">
+
+        <v-row class="mb-3 ml-3">
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn small depressed color="#fafafa" class="grey--text" @click="sortBy('title')" v-on="on">
+                <v-icon left small>mdi-folder</v-icon>
+                <span class="caption text-lowercase">By project name</span>
+              </v-btn>
+            </template>
+            <span>Sort projects by project name</span>
+          </v-tooltip>
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn small depressed color="#fafafa" class="grey--text" @click="sortBy('person')" v-on="on">
+                <v-icon left small>mdi-account</v-icon>
+                <span class="caption text-lowercase">By person</span>
+              </v-btn>
+            </template>
+            <span>Sort projects by person</span>
+          </v-tooltip>
+        </v-row>
+
         <v-card flat class="mx-4" v-for="project in projects" :key="project.title">
           <v-row :class="`pa-3 project ${project.status}`">
              <v-col cols="12" md="6">
@@ -42,6 +64,11 @@ export default {
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
     }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b [prop] ? -1 : 1)
+    } 
   }
 };
 </script>
